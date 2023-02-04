07e4ac258f0dc5c4fb3e921af9468ca18aa39f61
unknown <renancavalcantercb@protonmail.com>
2023-02-03 21:39:46 -0300

    fix .env and requirements

--- application/__init__.py
+++ application/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from os import getenv
+from os import environ
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SECRET_KEY"] = getenv("SECRET_KEY")
-app.config["MONGO_URI"] = getenv("MONGO_URI")
+app.config["SECRET_KEY"] = environ("SECRET_KEY")
+app.config["MONGO_URI"] = environ("MONGO_URI")
 
 client = MongoClient(app.config["MONGO_URI"], connectTimeoutMS=30000, socketTimeoutMS=None, connect=False,
                      maxPoolsize=1)
