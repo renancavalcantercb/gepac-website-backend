c88e5fead91595dde615e5c37abd659839f8ce52
unknown <renancavalcantercb@protonmail.com>
2023-02-03 21:43:21 -0300

    fix .env

--- application/__init__.py
+++ application/__init__.py
@@ -9,8 +9,8 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config["SECRET_KEY"] = environ("SECRET_KEY")
-app.config["MONGO_URI"] = environ("MONGO_URI")
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["MONGO_URI"] = environ.get("MONGO_URI")
 
 client = MongoClient(app.config["MONGO_URI"], connectTimeoutMS=30000, socketTimeoutMS=None, connect=False,
                      maxPoolsize=1)
